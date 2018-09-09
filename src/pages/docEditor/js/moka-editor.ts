f5cffcc495a9cd5dd14dfcb49ff932bd52fa2913
ArthurFree <lc940401@gmail.com>
2018-09-09 22:25:07 +0800

    update

--- src/pages/docEditor/js/moka-editor.ts
+++ src/pages/docEditor/js/moka-editor.ts
@@ -1,6 +1,15 @@
 import utils from '../../../utils';
 import markdown from './markdown';
 
+const doc: any = document;
+const commandsReg = {
+    block: /^(?:p|h[1-6]|blockquote|pre)$/,
+    inline: /^(?:bold|italic|underline|insertorderedlist|insertunorderedlist|indent|outdent)$/,
+    source: /^(?:createlink|unlink)$/,
+    insert: /^(?:inserthorizontalrule|insertimage|insert)$/,
+    wrap: /^(?:code)$/,
+};
+
 function addListener(ctx: any, target: any, type: string, listener: any): any {
     target.addEventListener(type, listener, false);
     return ctx;
@@ -8,7 +17,9 @@ function addListener(ctx: any, target: any, type: string, listener: any): any {
 
 export default class MokaEditor {
     public config: any;
+    public selection: any;
     public markdown = markdown;
+    private _range: any;
 
     constructor(options: any) {
         console.log('--- Editor ---');
@@ -19,11 +30,31 @@ export default class MokaEditor {
 
         this.config = opts;
 
+        if (doc.getSelection) {
+            this.selection = doc.getSelection();
+        }
+
         if (this.markdown) this.markdown.init(this);
     }
 
     public on(type: string, listener: any) {
         addListener(this, this.config.editor, type, listener);
         return this;
+    }
+
+    public getRange() {
+        // const editor = this.config.editor;
+        // let range = this.selection.rangeCount && this.selection.getRangeAt(0);
+    }
+
+    public setRange(range: any) {
+        range = range || this._range;
+        if (!range) {
+            range = this.getRange();
+        }
+    }
+
+    public execCommand(name: string, value?: any) {
+        name = name.toLowerCase();
     }
 }
